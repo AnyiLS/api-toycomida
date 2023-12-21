260508bafa498eef304e4cc3fa64ce0bdc3558f8
Anyi Lozano <tdprogramacioneml5@gmail.com>
2023-12-21 16:30:11 -0500

    borrar js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var api_1 = require("./routes/api");
-var bodyParser = require("body-parser");
-var app = (0, express_1.default)();
-app.use(bodyParser.json());
-app.use("/api", api_1.default);
-app.listen(3000);
--- src/routes/api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MailerController_1 = require("../controllers/MailerController");
-var express_1 = require("express");
-var route = (0, express_1.default)();
-route.post("/sent-email", new MailerController_1.default().sendEmail);
-exports.default = route;
