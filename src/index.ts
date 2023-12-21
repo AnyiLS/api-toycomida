e59ef658244717153b80c05a86815f0a44964bf1
Anyi Lozano <tdprogramacioneml5@gmail.com>
2023-12-21 16:33:30 -0500

    vvdgvd

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,6 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use("/api", route);
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
